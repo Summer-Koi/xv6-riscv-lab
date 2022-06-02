e85a83054117a75740ade4b6619c85c15584e627
Chlamydomonos <2457242458@qq.com>
2022-06-02 20:46:10 +0800

    Added some files and changed NBUF to 31

--- kernel/param.h
+++ kernel/param.h
@@ -8,6 +8,8 @@
 #define MAXARG       32  // max exec arguments
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+
+//修改Buffer数量为31个，以使用双向平方探测法的哈希表存储
+#define NBUF         (MAXOPBLOCKS*3) + 1 // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
